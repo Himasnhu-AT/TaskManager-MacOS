3334534956bd4a0c70a133045d1d2d52c59a913c
Himanshu-AT <aristech.official@gmail.com>
2024-07-14 02:37:15 +0530

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Himanshu on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
