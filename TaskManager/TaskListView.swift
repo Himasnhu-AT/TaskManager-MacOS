73834b56d7d0d6db8121c001dccba5214f4b2e47
Himanshu-AT <aristech.official@gmail.com>
2024-07-14 03:33:14 +0530

    Sample UI completion

---
+++ TaskManager/TaskListView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskListView.swift
+//  TaskManager
+//
+//  Created by Himanshu on 7/14/24.
+//
+
+import SwiftUI
+
+struct TaskListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskListView()
+}
